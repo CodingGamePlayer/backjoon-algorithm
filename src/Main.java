5301fee5b89f6152bf6de7bc3baa6d2c807dc77b
김종식 <otw1917@naver.com>
2023-03-14 15:58:54 +0900

    1021/회전하는 큐

--- src/Main.java
+++ src/Main.java
@@ -1,53 +1,52 @@
 package src;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
-        scanner.nextLine();
-        int[] arr = Arrays.stream(scanner.nextLine().split(" "))
-                .mapToInt(Integer::parseInt)
-                .toArray();
+        String s1 = scanner.nextLine();
+        String s2 = scanner.nextLine();
 
-        if (check(a, arr)) {
-            System.out.println("YES");
-        } else {
-            System.out.println("NO");
-        }
+        int[] arrA = Arrays.stream(s1.split(" ")).mapToInt(Integer::parseInt).toArray();
+        int[] arrB = Arrays.stream(s2.split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        System.out.println(check(arrA, arrB));
     }
 
-    public static boolean check(int a, int[] arr) {
+    public static int check(int[] arrA, int[] arrB) {
+        int count = 0;
+        int target;
+        LinkedList<Integer> deque = new LinkedList<>();
+        int[] arrFind = arrB.clone();
 
-        ArrayList<Stack> arrayList = new ArrayList<>(Arrays.asList(
-                new Stack<>(),
-                new Stack<>(),
-                new Stack<>(),
-                new Stack<>()
-        ));
-
-
-        for (int i = 0; i < a; i++) {
-            int point = 1;
-            for (int j = 0; j < arrayList.size(); j++) {
-                if (arrayList.get(j).isEmpty()) {
-                    arrayList.get(j).push(arr[i]);
-                    break;
-                }
-                int peek = (int) arrayList.get(j).peek();
-                if (peek > arr[i]) {
-                    if (point >= arrayList.size()) {
-                        return false;
-                    }
-                    point++;
-                } else {
-                    arrayList.get(j).push(arr[i]);
-                    break;
-                }
-            }
+        for (int i = 1; i <= arrA[0]; i++) {
+            deque.offer(i);
         }
 
-        return true;
+
+        for (int i = 0; i < arrFind.length; i++) {
+            target = deque.indexOf(arrFind[i]);
+            int half = deque.size() / 2;
+            if (target <= half) {
+                while (target != 0) {
+                    deque.offerLast(deque.pollFirst());
+                    target--;
+                    count++;
+                }
+            } else {
+                while (target != deque.size()) {
+                    deque.offerFirst(deque.pollLast());
+                    target++;
+                    count++;
+                }
+            }
+            deque.pollFirst();
+        }
+
+
+        return count;
     }
 }
