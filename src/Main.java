3662b407548f147c303ddc7be695a69c1ad4754b
김종식 <otw1917@naver.com>
2023-03-15 19:51:47 +0900

    10818/최소, 최대

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 package src;
 
 import java.util.Arrays;
-import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Main {
@@ -10,43 +9,15 @@ public class Main {
         String s1 = scanner.nextLine();
         String s2 = scanner.nextLine();
 
-        int[] arrA = Arrays.stream(s1.split(" ")).mapToInt(Integer::parseInt).toArray();
+        int arrA = Integer.parseInt(s1);
         int[] arrB = Arrays.stream(s2.split(" ")).mapToInt(Integer::parseInt).toArray();
-
-        System.out.println(check(arrA, arrB));
+        int min = Arrays.stream(arrB).min().orElse(0);
+        int max = Arrays.stream(arrB).max().orElse(0);
+        System.out.printf("%d %d", min, max);
     }
 
-    public static int check(int[] arrA, int[] arrB) {
-        int count = 0;
-        int target;
-        LinkedList<Integer> deque = new LinkedList<>();
-        int[] arrFind = arrB.clone();
-
-        for (int i = 1; i <= arrA[0]; i++) {
-            deque.offer(i);
-        }
-
-
-        for (int i = 0; i < arrFind.length; i++) {
-            target = deque.indexOf(arrFind[i]);
-            int half = deque.size() / 2;
-            if (target <= half) {
-                while (target != 0) {
-                    deque.offerLast(deque.pollFirst());
-                    target--;
-                    count++;
-                }
-            } else {
-                while (target != deque.size()) {
-                    deque.offerFirst(deque.pollLast());
-                    target++;
-                    count++;
-                }
-            }
-            deque.pollFirst();
-        }
-
-
-        return count;
-    }
+//    public static int[] check(int arrA, int[] arrB) {
+//
+//        return new int[] {min,max};
+//    }
 }
