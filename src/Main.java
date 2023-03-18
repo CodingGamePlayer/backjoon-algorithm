08fd05bb6952705e2ef4177b9f8a2b385e7a0ca6
KJS <otw1917@naver.com>
2023-03-19 03:34:41 +0900

    24174/알고리즘 수업 - 힙 정렬 2

--- src/Main.java
+++ src/Main.java
@@ -1,99 +1,75 @@
 package src;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
-import java.util.function.ToIntFunction;
 
 public class Main {
+    static int count = 0;
+    static int LIMIT;
+
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
         String[] arr1 = sc.nextLine().split(" ");
         String[] arr2 = sc.nextLine().split(" ");
         int N = Integer.parseInt(arr1[0]);
-        int K = Integer.parseInt(arr1[1]);
+        LIMIT = Integer.parseInt(arr1[1]);
         int[] ints = Arrays.stream(arr2).mapToInt(Integer::parseInt).toArray();
-        solution(N, K, ints);
+
+        heapSort(ints);
+        System.out.println(-1);
     }
 
+    private static void heapSort(int[] ints) {
+        int len = ints.length;
+        buildHeap(ints, len); // 최소힙을 생성하는 메소드
 
-    private static void solution(int N, int K, int[] arr2) {
-        int[] minHeap = new int[N + 1];
-        int count = 0;
-        int limit = K;
-
-        for (int i = 1; i < minHeap.length; i++) {
-
-            int cur = i;
-            minHeap[i] = arr2[i - 1];
-            if (limit > 0){
-
-                while (cur > 1 && minHeap[cur / 2] > minHeap[cur]) {
-                    int parentValue = minHeap[cur / 2];
-                    minHeap[cur / 2] = minHeap[cur];
-                    minHeap[cur] = parentValue;
-
-                    cur /= 2;
-                    count++;
-                    limit--;
-
-                }
-            }
+        for (int i = len - 1; i >= 0; i--) {
+            swap(ints, 0, i);
+            heapify(ints, 0, i);
         }
-        if (limit < 0) {
-            System.out.println(-1);
+    }
+
+    private static void buildHeap(int[] ints, int len) {
+        for (int i = len / 2 - 1; i >= 0; i--) {
+            heapify(ints, i, len);
+        }
+    }
+
+    private static void heapify(int[] ints, int i, int len) {
+        int leftIdx = i * 2 + 1;
+        int rightIdx = i * 2 + 2;
+        int targetIdx = -1;
+
+        if (rightIdx < len) {
+            targetIdx = ints[leftIdx] < ints[rightIdx] ? leftIdx : rightIdx;
+        } else if (leftIdx < len) {
+            targetIdx = leftIdx;
         } else {
-            for (int i = 1; i < minHeap.length; i++) {
-                if (i < minHeap.length - 1) {
-                    System.out.print(minHeap[i] + " ");
-                } else {
-                    System.out.print(minHeap[i]);
-                }
-            }
-            System.out.println();
+            return;
+        }
+
+        if (ints[targetIdx] < ints[i]) {
+            swap(ints, i, targetIdx);
+            heapify(ints, targetIdx, len);
         }
     }
 
+    private static void swap(int[] ints, int i, int targetIdx) {
+        if (count++ < LIMIT) {
+            int parentVal = ints[i];
+            ints[i] = ints[targetIdx];
+            ints[targetIdx] = parentVal;
+
+
+        }
+        if (count == LIMIT + 1) {
+            StringBuffer sb = new StringBuffer();
+            Arrays.stream(ints).forEach(s -> sb.append(s).append(" "));
+            System.out.println(sb);
+            System.exit(0);
+        }
+
+    }
 
 }
-//            if (K > 0) {
-//                    int cur = i;
-//                    minHeap[i] = arr2[i - 1];
-//
-//                    while (cur > 1 && minHeap[cur / 2] > minHeap[cur]) {
-//                    int parentValue = minHeap[cur / 2];
-//                    minHeap[cur / 2] = minHeap[cur];
-//                    minHeap[cur] = parentValue;
-//
-//                    cur /= 2;
-//                    count++;
-//                    }
-//
-//                    while (true) {
-//                    int leftIdx = cur * 2;
-//                    int rightIdx = cur * 2 + 1;
-//                    int targetIdx = -1;
-//
-//                    if (rightIdx < minHeap.length) {
-//        targetIdx = minHeap[leftIdx] < minHeap[rightIdx] ? leftIdx : rightIdx;
-//        } else if (leftIdx < minHeap.length) {
-//        targetIdx = leftIdx;
-//        } else {
-//        break;
-//        }
-//
-//        if (minHeap[cur] < minHeap[targetIdx]) {
-//        break;
-//        } else {
-//        int parentValue = minHeap[cur];
-//        minHeap[targetIdx] = parentValue;
-//        minHeap[cur] = minHeap[targetIdx];
-//        cur = targetIdx;
-//        count++;
-//        }
-//        }
-//
-//        } else {
-//        minHeap[i] = arr2[i - 1];
-//        }
