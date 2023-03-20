cc2ba3827882288838460303ef70eeab046199d2
김종식 <otw1917@naver.com>
2023-03-20 19:17:41 +0900

    2830/행성 X3

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,33 @@
 package src;
 
-import java.io.IOException;
 import java.util.*;
 
 public class Main {
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        int count = sc.nextInt();
-        sc.nextLine();
-        int[] s = Arrays.stream(sc.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        int target = sc.nextInt();
-        sc.nextLine();
-
-        solution(s, target);
-
-    }
-
-    private static void solution(int[] s, int target) {
-
-        Hashtable<Integer, Integer> map = new Hashtable<>();
-
-        for (int i : s) {
-            int num = (int) map.getOrDefault(i, 0);
-            map.put(i, ++num);
+        int n = sc.nextInt();
+        int[] v = new int[n];
+        for (int i = 0; i < n; i++) {
+            v[i] = sc.nextInt();
         }
 
-        if (map.get(target) != null)
-            System.out.println(map.get(target));
-        else
-            System.out.println(0);
+        int[] bcnt = new int[20]; // 각 자리수별로 1이 몇 개 있는지
+
+        for (int i = 0; i < 20; i++) {
+            for (int j = 0; j < n; j++) {
+                if ((v[j] & (1 << i)) != 0) {
+                    bcnt[i]++;
+                }
+            }
+        }
+
+        long ans = 0;
+
+        for (int i = 0; i < 20; i++) {
+            ans += (1L << i) * bcnt[i] * (n - bcnt[i]);
+        }
+
+        System.out.println(ans);
     }
-
-
 }
+
