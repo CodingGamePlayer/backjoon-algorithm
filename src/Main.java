73356e435edc3471133e4f259b60793144c69a0f
KJS <otw1917@naver.com>
2023-04-03 10:43:56 +0900

    1254/2차원 배열의 합

--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,44 @@
 package src;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Scanner scan = new Scanner(System.in);
-        String s = scan.nextLine();
-        int len = s.length();
-        int answer = len;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        for (int i = 0; i < len; i++) {
-            if (!solution(s.substring(i))){
-                answer++;
-            } else {
-                break;
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[][] arr = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        System.out.println(answer);;
+        int k = Integer.parseInt(br.readLine());
 
-    }
+        while (k > 0) {
+            k--;
+            st = new StringTokenizer(br.readLine());
+            int i = Integer.parseInt(st.nextToken());
+            int j = Integer.parseInt(st.nextToken());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
 
-    static boolean solution(String s) {
-        int left = 0;
-        int right = s.length() -1;
-
-        while (left <= right) {
-            if (s.charAt(left) == s.charAt(right)){
-                left++;
-                right--;
-            } else {
-                return false;
+            int sum = 0;
+            for (int l = i - 1; l < x; l++) {
+                for (int m = j -1; m < y; m++) {
+                    sum += arr[l][m];
+                }
             }
+            System.out.println(sum);
         }
-
-        return true;
     }
+
 }
