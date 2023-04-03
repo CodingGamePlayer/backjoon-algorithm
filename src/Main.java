64a1ac62f5a50df848589da09139dcc8ffcf6a83
KJS <otw1917@naver.com>
2023-04-03 13:19:47 +0900

    11047/동전 0

--- src/Main.java
+++ src/Main.java
@@ -1,46 +1,28 @@
 package src;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
-    static int answer = 0;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int money = sc.nextInt();
+        int answer = 0;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] coins = new int[n];
 
-        int n = Integer.parseInt(st.nextToken());
-        int[][] arr = new int[n][n];
+        for (int i = 0; i < coins.length; i++) {
+            coins[i] = sc.nextInt();
+        }
 
-        for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < n; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
+        for (int i = coins.length-1; i >= 0 ; i--) {
+            if (money / coins[i] > 0) {
+                answer += money / coins[i];
+                money %= coins[i];
             }
         }
 
-        dp(arr, n, 0, 0);
         System.out.println(answer);
-
     }
-
-    private static void dp(int[][] arr, int n, int row, int col) {
-        if (row >= n || col >= n) {
-            return;
-        }
-
-        if (arr[row][col] == 0) {
-            answer++;
-            return;
-        }
-
-        dp(arr, n, row + arr[row][col], col);
-        dp(arr, n , row, col + arr[row][col]);
-
-    }
-
-
 }
+
