692dfe174d1676ad4c783de4a0d051b1068bbbdf
KJS <otw1917@naver.com>
2023-04-07 22:36:18 +0900

    2839/설탕 문제

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,32 @@
 package src;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int money = sc.nextInt();
-        int answer = 0;
 
-        int[] coins = new int[n];
+        System.out.println(solution(n));
+    }
 
-        for (int i = 0; i < coins.length; i++) {
-            coins[i] = sc.nextInt();
-        }
+    private static int solution(int n) {
+        int[] dp = new int[n + 1];
 
-        for (int i = coins.length-1; i >= 0 ; i--) {
-            if (money / coins[i] > 0) {
-                answer += money / coins[i];
-                money %= coins[i];
+        for (int i = 1; i <= n; i++) {
+            dp[i] = Integer.MAX_VALUE;
+
+            if (i - 3 >= 0 && dp[i - 3] != Integer.MAX_VALUE) {
+                dp[i] = Math.min(dp[i], dp[i - 3] + 1);
+            }
+
+            if (i - 5 >= 0 && dp[i - 5] != Integer.MAX_VALUE) {
+                dp[i] = Math.min(dp[i], dp[i - 5] + 1);
             }
         }
 
-        System.out.println(answer);
+        return dp[n] == Integer.MAX_VALUE ? -1 : dp[n];
     }
 }
 
