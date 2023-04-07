9ec07d3eb1d2c1e521f870aac5f6cec77c4f3be9
KJS <otw1917@naver.com>
2023-04-08 00:36:54 +0900

    1463/1,2,3 더하기

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,36 @@
 package src;
 
-import java.util.Arrays;
+
+import java.io.IOException;
 import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
 
-        System.out.println(solution(n));
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int num = sc.nextInt();
+            System.out.println(solution(num));
+        }
+
     }
 
     private static int solution(int n) {
         int[] dp = new int[n + 1];
+        dp[1] = 1;
 
-        for (int i = 2; i <= n; i++) {
-            dp[i] = dp[i - 1] + 1;
-
-            if (i % 2 == 0) {
-                dp[i] = Math.min(dp[i], dp[i / 2] + 1);
-            }
-
-            if (i % 3 == 0) {
-                dp[i] = Math.min(dp[i], dp[i / 3] + 1);
-            }
+        if (n >= 2) {
+            dp[2] = 2;
+        }
+        if (n >= 3) {
+            dp[3] = 4;
         }
 
+        for (int i = 4; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+        }
         return dp[n];
     }
 }
