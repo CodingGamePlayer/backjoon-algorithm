3e95ab15b472c260c19e15c01853e93b914216c9
KJS <otw1917@naver.com>
2023-04-07 23:08:22 +0900

    1463/1로 만들기

--- src/Main.java
+++ src/Main.java
@@ -14,19 +14,19 @@ public class Main {
     private static int solution(int n) {
         int[] dp = new int[n + 1];
 
-        for (int i = 1; i <= n; i++) {
-            dp[i] = Integer.MAX_VALUE;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + 1;
 
-            if (i - 3 >= 0 && dp[i - 3] != Integer.MAX_VALUE) {
-                dp[i] = Math.min(dp[i], dp[i - 3] + 1);
+            if (i % 2 == 0) {
+                dp[i] = Math.min(dp[i], dp[i / 2] + 1);
             }
 
-            if (i - 5 >= 0 && dp[i - 5] != Integer.MAX_VALUE) {
-                dp[i] = Math.min(dp[i], dp[i - 5] + 1);
+            if (i % 3 == 0) {
+                dp[i] = Math.min(dp[i], dp[i / 3] + 1);
             }
         }
 
-        return dp[n] == Integer.MAX_VALUE ? -1 : dp[n];
+        return dp[n];
     }
 }
 
