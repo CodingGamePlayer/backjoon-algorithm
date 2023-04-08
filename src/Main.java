5fb04ab0ab269ea1fbfa68e3cbc155aa9b34c68b
KJS <otw1917@naver.com>
2023-04-09 03:12:50 +0900

    1780/종이의 개수

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,19 @@ package src;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
+    static int minus = 0;
+    static int zero = 0;
+    static int plus = 0;
+
+    static int[][] arr;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[][] arr = new int[n][n];
+        arr = new int[n][n];
 
         // 입력 받기
         for (int i = 0; i < n; i++) {
@@ -21,26 +25,38 @@ public class Main {
                 arr[i][idx++] = Integer.parseInt(st.nextToken());
             }
         }
-
-        System.out.println(bfs(arr, n));
+        dac(0, 0, n);
+        System.out.println(minus);
+        System.out.println(zero);
+        System.out.println(plus);
     }
 
-    private static int bfs(int[][] arr, int n) {
-        int[][] dp = new int[n][n];
+    private static void dac(int x, int y, int size) {
+        int num = arr[x][y];
+        boolean sameNum = true;
 
-        dp[0] = arr[0].clone();
-
-        for (int i = 1; i < dp.length; i++) {
-            for (int j = 0; j < i + 1; j++) {
-                int left = arr[i][j] + dp[i - 1][Math.max(0, j - 1)];
-                int right = arr[i][j] + dp[i - 1][j];
-                dp[i][j] = Math.max(left, right);
+        for (int i = x; i < x + size; i++) {
+            for (int j = y; j < y + size; j++) {
+                if (num != arr[i][j]) {
+                    sameNum = false;
+                    break;
+                }
             }
+            if (!sameNum) break;
         }
 
-        return Arrays.stream(dp[n - 1]).max().orElse(0);
+        if (sameNum) {
+            if (num == -1) minus++;
+            else if (num == 0) zero++;
+            else plus++;
+        } else {
+            int n = size / 3;
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    dac(x + i * n, y + j * n, n);
+                }
+            }
+        }
     }
-
-
 }
 
