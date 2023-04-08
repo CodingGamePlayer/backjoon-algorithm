fb737407d76d93550cd318dd6966f8ffdda392fc
KJS <otw1917@naver.com>
2023-04-08 22:32:25 +0900

    2630/색종이 만들기

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,62 @@
 package src;
 
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Scanner;
-
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
+    static int[][] matrix;
+    static int blue = 0;
+    static int white = 0;
+
     public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        while (t-- > 0) {
-            int dong = sc.nextInt();
-            int ho = sc.nextInt();
-            System.out.println(solution(dong, ho));
-        }
+        int N = Integer.parseInt(br.readLine());
 
-    }
+        matrix = new int[N][N];
 
-    private static int solution(int dong, int ho) {
-        int[][] dp = new int[dong + 1][ho + 1];
-
-        for (int i = 0; i < dp[0].length; i++) {
-            dp[0][i] = i;
-        }
-        for (int i = 0; i < dp.length - 1; i++) {
-            for (int j = 1; j < dp[i].length; j++) {
-                for (int k = 1; k <= j; k++) {
-                    dp[i + 1][j] += dp[i][k];
-                }
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                matrix[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        return dp[dong][ho];
+        cut(0, 0, N);
+        System.out.println(white);
+        System.out.println(blue);
+    }
+
+    public static void cut(int x, int y, int size) {
+        int color = matrix[x][y]; // 시작점의 색상
+        boolean sameColor = true;
+        for (int i = x; i < x + size; i++) {
+            for (int j = y; j < y + size; j++) {
+                if (matrix[i][j] != color) { // 색상이 다르면 분할
+                    sameColor = false;
+                    break;
+                }
+            }
+            if (!sameColor) {
+                break;
+            }
+        }
+        if (sameColor) { // 같은 색상이면 종이 개수 증가
+            if (color == 0) {
+                white++;
+            } else {
+                blue++;
+            }
+        } else { // 다른 색상이면 분할
+            int newSize = size / 2;
+            cut(x, y, newSize);
+            cut(x, y + newSize, newSize);
+            cut(x + newSize, y, newSize);
+            cut(x + newSize, y + newSize, newSize);
+        }
     }
 }
 
