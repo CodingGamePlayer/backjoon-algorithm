dc9486f552e18e1275a75f9d8eb2abe5e49912e7
KJS <otw1917@naver.com>
2023-04-09 00:15:11 +0900

    1932/정수 삼각형

--- src/Main.java
+++ src/Main.java
@@ -3,54 +3,44 @@ package src;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int[][] arr;
-    static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        arr = new int[n][n];
+        int[][] arr = new int[n][n];
 
         // 입력 받기
         for (int i = 0; i < n; i++) {
-            String str = br.readLine();
-            for (int j = 0; j < n; j++) {
-                arr[i][j] = str.charAt(j) - '0';
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int idx = 0;
+            while (st.hasMoreTokens()) {
+                arr[i][idx++] = Integer.parseInt(st.nextToken());
             }
         }
 
-        divide(0, 0, n);
-
-        System.out.println(sb.toString());
+        System.out.println(bfs(arr, n));
     }
 
-    public static void divide(int x, int y, int n) {
-        int color = arr[x][y];
-        boolean sameColor = true;
+    private static int bfs(int[][] arr, int n) {
+        int[][] dp = new int[n][n];
 
-        for (int i = x; i < x + n; i++) {
-            for (int j = y; j < y + n; j++) {
-                if (color != arr[i][j]) {
-                    sameColor = false;
-                    break;
-                }
+        dp[0] = arr[0].clone();
+
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 0; j < i + 1; j++) {
+                int left = arr[i][j] + dp[i - 1][Math.max(0, j - 1)];
+                int right = arr[i][j] + dp[i - 1][j];
+                dp[i][j] = Math.max(left, right);
             }
-            if (!sameColor) break;
         }
 
-        if (sameColor) {
-            sb.append(color);
-        } else {
-            sb.append("(");
-            int newSize = n / 2;
-            divide(x, y, newSize);
-            divide(x, y + newSize, newSize);
-            divide(x + newSize, y, newSize);
-            divide(x + newSize, y + newSize, newSize);
-            sb.append(")");
-        }
+        return Arrays.stream(dp[n - 1]).max().orElse(0);
     }
+
+
 }
 
