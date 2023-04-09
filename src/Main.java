68b97b2709e192708f7b4d3c2a228c381150dc60
KJS <otw1917@naver.com>
2023-04-10 01:38:51 +0900

    9663/N-Queen

--- src/Main.java
+++ src/Main.java
@@ -6,38 +6,37 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int[] arr;
-    static boolean[] visited;
-    public static void main(String[] args) throws IOException {
+    static int n;
+    static int[] cols;
+    static int count = 0;
+
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-
-        arr = new int[n];
-        visited = new boolean[n];
-
-        solution(n, m, 0);
+        n = Integer.parseInt(br.readLine());
+        cols = new int[n];
+        backtracking(0);
+        System.out.println(count);
     }
 
-    private static void solution(int n, int m, int depth) {
-        if (depth == m) {
-            for (int i = 0; i < m; i++) {
-                System.out.print(arr[i] + " ");
-            }
-            System.out.println();
+    static void backtracking(int row) {
+        if (row == n) {
+            count++;
             return;
         }
 
         for (int i = 0; i < n; i++) {
-            if (!visited[i]){
-                visited[i] = true;
-                arr[depth] = i + 1;
-                solution(n, m, depth + 1);
-                visited[i] = false;
+            cols[row] = i;
+            if (isPossible(row)){
+                backtracking(row + 1);
             }
         }
+    }
+
+    static boolean isPossible(int row) {
+        for (int i = 0; i < row; i++) {
+            if (cols[i] == cols[row] || Math.abs(cols[row] - cols[i]) == row - i) return false;
+        }
+        return true;
     }
 }
 
