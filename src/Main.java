f65f33043abacbc18ab52922f3dd381c29a115ef
KJS <otw1917@naver.com>
2023-04-10 00:14:18 +0900

    15649/N과 M(1) - 풀이 visited

--- src/Main.java
+++ src/Main.java
@@ -6,38 +6,37 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-
+    static int[] arr;
+    static boolean[] visited;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int[][] cost = new int[n][3]; // RGB 색상 비용을 저장할 배열
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
-        // 입력받은 색상 비용 저장
+        arr = new int[n];
+        visited = new boolean[n];
+
+        solution(n, m, 0);
+    }
+
+    private static void solution(int n, int m, int depth) {
+        if (depth == m) {
+            for (int i = 0; i < m; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+        }
+
         for (int i = 0; i < n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < 3; j++) {
-                cost[i][j] = Integer.parseInt(st.nextToken());
+            if (!visited[i]){
+                visited[i] = true;
+                arr[depth] = i + 1;
+                solution(n, m, depth + 1);
+                visited[i] = false;
             }
         }
-
-        // 다이나믹 프로그래밍으로 최소 비용 계산
-        int[][] dp = new int[n][3]; // 각 집을 해당 색으로 칠할 때까지의 최소 비용을 저장할 배열
-        dp[0][0] = cost[0][0]; // 첫 번째 집을 빨간색으로 칠할 때 최소 비용
-        dp[0][1] = cost[0][1]; // 첫 번째 집을 초록색으로 칠할 때 최소 비용
-        dp[0][2] = cost[0][2]; // 첫 번째 집을 파란색으로 칠할 때 최소 비용
-
-        for (int i = 1; i < n; i++) {
-            // i번째 집을 빨간색으로 칠할 때의 최소 비용 계산
-            dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + cost[i][0];
-            // i번째 집을 초록색으로 칠할 때의 최소 비용 계산
-            dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + cost[i][1];
-            // i번째 집을 파란색으로 칠할 때의 최소 비용 계산
-            dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + cost[i][2];
-        }
-
-        // n번째 집까지 칠하는데 드는 최소 비용 출력
-        System.out.println(Math.min(dp[n - 1][0], Math.min(dp[n - 1][1], dp[n - 1][2])));
     }
 }
 
