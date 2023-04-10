bb90f1a7a53eedc8d4cf5ead3c67e610cee5126c
KJS <otw1917@naver.com>
2023-04-11 02:00:07 +0900

    10162/전자레인지

--- src/Main.java
+++ src/Main.java
@@ -1,47 +1,27 @@
 package src;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.LinkedList;
 import java.util.Scanner;
-import java.util.stream.Collectors;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[] arr1 = new int[n];
-        int[] arr2 = new int[n];
 
-        for (int i = 0; i < n; i++) {
-            arr1[i] = sc.nextInt();
+        if (n % 10 != 0) {
+            System.out.println(-1);
+        } else {
+            int[] arr = {300, 60, 10};
+
+            int[] ans = new int[3];
+            for (int i = 0; i < arr.length; i++) {
+                if (n / arr[i] > 0) {
+                    ans[i] = n / arr[i];
+                    n %= arr[i];
+                }
+                System.out.print(ans[i] + " ");
+            }
         }
-
-        for (int i = 0; i < n; i++) {
-            arr2[i] = sc.nextInt();
-        }
-
-        solution(arr1, arr2, n);
-
-    }
-
-    private static void solution(int[] arr1, int[] arr2, int n) {
-        LinkedList<Integer> listA = new LinkedList<>(Arrays.stream(arr1).boxed().collect(Collectors.toList()));
-        LinkedList<Integer> listB = new LinkedList<>(Arrays.stream(arr2).boxed().collect(Collectors.toList()));
-        Collections.sort(listA);
-        Collections.sort(listB, Collections.reverseOrder());
-
-        int sum = 0;
-        while (n > 0){
-            int a = listA.poll();
-            int b = listB.poll();
-
-            sum += a * b;
-
-        }
-
-        System.out.println(sum);
     }
 }
 
