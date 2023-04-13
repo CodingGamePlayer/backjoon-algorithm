a078bcb67f2539b0daa82cdce3e1b6e71a776ea6
KJS <otw1917@naver.com>
2023-04-14 04:30:08 +0900

    11866/요세푸스 문제0

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 package src;
 
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
@@ -11,11 +11,33 @@ public class Main {
         int a = sc.nextInt();
         int b = sc.nextInt();
 
-        System.out.println(a + b);
-        System.out.println(a - b);
-        System.out.println(a * b);
-        System.out.println(a / b);
-        System.out.println(a % b);
+        Deque<Integer> deque = new LinkedList<>();
+        for (int i = 1; i <= a; i++) {
+            deque.offer(i);
+        }
+
+        ArrayList<Integer> answer = new ArrayList<>();
+
+        int count = 1;
+        while (!deque.isEmpty()) {
+            if (count % b == 0) {
+                answer.add(deque.poll());
+            } else {
+                deque.offer(deque.poll());
+            }
+            count++;
+        }
+        StringBuilder sb = new StringBuilder();
+        sb.append("<");
+        for (int i = 0; i < answer.size(); i++) {
+            if (i == answer.size() - 1) {
+                sb.append(answer.get(i) + ">");
+            } else {
+                sb.append(answer.get(i) + ", ");
+            }
+        }
+
+        System.out.println(sb.toString());
     }
 
 }
