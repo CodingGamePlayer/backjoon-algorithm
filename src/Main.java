76ab1785d2d30416bfd6ffed8e27544b94730436
KJS <otw1917@naver.com>
2023-04-14 00:33:13 +0900

    14888/연산자 끼워넣기

--- src/Main.java
+++ src/Main.java
@@ -4,30 +4,48 @@ import java.util.Scanner;
 
 public class Main {
 
+    static int n;
+    static int[] nums;
+    static int[] op = new int[4];
 
+    static int max = Integer.MIN_VALUE, min = Integer.MAX_VALUE;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String s = sc.nextLine();
+        n = sc.nextInt();
 
-        String[] splitA = s.split("[-]");
+        nums = new int[n];
 
-        int ans = 0;
-        for (int i = 0; i < splitA.length; i++) {
-            String[] splitB = splitA[i].split("[+]");
-
-            int sum = 0;
-            for (int j = 0; j < splitB.length; j++) {
-                sum += Integer.parseInt(splitB[j]);
-            }
-
-            if (i == 0) {
-                ans = sum;
-            } else {
-                ans -= sum;
-            }
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
         }
 
-        System.out.println(ans);
+        for (int i = 0; i < 4; i++) {
+            op[i] = sc.nextInt();
+        }
+
+        dfs(nums[0], 1);
+
+        System.out.println(max);
+        System.out.println(min);
+    }
+
+    private static void dfs(int num, int depth) {
+        if (depth == n) {
+            max = Math.max(max, num);
+            min = Math.min(min, num);
+            return;
+        }
+
+        for (int i = 0; i < 4; i++) {
+            if (op[i] > 0){
+                op[i]--;
+                if (i == 0) dfs(num + nums[depth], depth + 1);
+                else if (i == 1) dfs(num - nums[depth], depth + 1);
+                else if (i == 2) dfs(num * nums[depth], depth + 1);
+                else dfs(num / nums[depth], depth + 1);
+                op[i]++;
+            }
+        }
     }
 }
 
