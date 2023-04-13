d232faa73f606bf8e9340ffcb1495fba713b42a5
KJS <otw1917@naver.com>
2023-04-14 04:20:19 +0900

    10869/사칙연산

--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,16 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        System.out.println("|\\_/|");
-        System.out.println("|q p|   /}");
-        System.out.println("( 0 )\"\"\"\\");
-        System.out.println("|\"^\"`    |");
-        System.out.println("||_/=\\\\__|");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
     }
 
 }
