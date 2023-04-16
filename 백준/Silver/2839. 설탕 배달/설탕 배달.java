6bf021e4e98be3c0ee8a274019bdc8b3eb9562eb
Kr.James <115041502+CodingGamePlayer@users.noreply.github.com>
2023-04-16 15:41:48 +0900

    [Silver IV] Title: 설탕 배달, Time: 108 ms, Memory: 12888 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,28 +1,31 @@
+import java.io.*;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
+
         int n = sc.nextInt();
 
-        System.out.println(solution(n));
-    }
-
-    private static int solution(int n) {
         int[] dp = new int[n + 1];
 
-        for (int i = 1; i <= n; i++) {
+        dp[0] = 0;
+        if (n > 1) dp[1] = Integer.MAX_VALUE;
+        if (n > 2) dp[2] = Integer.MAX_VALUE;
+
+        for (int i = 3; i <= n ; i++) {
             dp[i] = Integer.MAX_VALUE;
 
-            if (i - 3 >= 0 && dp[i - 3] != Integer.MAX_VALUE) {
-                dp[i] = Math.min(dp[i], dp[i - 3] + 1);
+            if (i >= 3 && dp[i - 3] != Integer.MAX_VALUE) {
+                dp[i] = dp[i - 3] + 1;
             }
-
-            if (i - 5 >= 0 && dp[i - 5] != Integer.MAX_VALUE) {
-                dp[i] = Math.min(dp[i], dp[i - 5] + 1);
+            if (i >= 5 && dp[i - 5] != Integer.MAX_VALUE) {
+                dp[i] = Math.min(dp[i], dp[i -5] + 1);
             }
         }
 
-        return dp[n] == Integer.MAX_VALUE ? -1 : dp[n];
+        System.out.println(dp[n] == Integer.MAX_VALUE ? -1 : dp[n]);
     }
 }
+
