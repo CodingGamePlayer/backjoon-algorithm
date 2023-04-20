c4315627d346d48d1bcf78daccab0a8b79e630a4
Kr.James <115041502+CodingGamePlayer@users.noreply.github.com>
2023-04-21 08:21:06 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 1912 ms, Memory: 93028 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -3,56 +3,58 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
-import java.util.StringTokenizer;
 
 public class Main {
+
+    static List<Integer>[] graph;
+    static int[] parents;
+    static boolean[] visited;
     static int n;
-    static ArrayList<Integer>[] tree;
-    static int[] parent;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         n = Integer.parseInt(br.readLine());
 
-        tree = new ArrayList[n + 1];
-        for (int i = 1; i <= n; i++) {
-            tree[i] = new ArrayList<>();
-        }
+        graph = new ArrayList[n + 1];
+        parents = new int[n + 1];
+        visited = new boolean[n + 1];
 
-        parent = new int[n + 1];
+        for (int i = 0; i < n + 1; i++) {
+            graph[i] = new ArrayList<>();
+        }
 
         for (int i = 0; i < n - 1; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int u = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
+            String[] s = br.readLine().split(" ");
+            int u = Integer.parseInt(s[0]);
+            int v = Integer.parseInt(s[1]);
 
-            tree[u].add(v);
-            tree[v].add(u);
+            graph[u].add(v);
+            graph[v].add(u);
         }
 
-        bfs();
+        bfs(1);
 
-        for (int i = 2; i <= n; i++) {
-            System.out.println(parent[i]);
+        for (int i = 2; i < n + 1; i++) {
+            System.out.println(parents[i]);
         }
+
     }
 
-    static void bfs() {
-        boolean[] visited = new boolean[n + 1];
-        Queue<Integer> q = new LinkedList<>();
+    private static void bfs(int root) {
+        Queue<Integer> queue = new LinkedList<>();
 
-        visited[1] = true;
-        q.offer(1);
-
-        while (!q.isEmpty()) {
-            int node = q.poll();
-
-            for (int child : tree[node]) {
-                if (!visited[child]) {
-                    visited[child] = true;
-                    parent[child] = node;
-                    q.offer(child);
+        queue.add(root);
+        visited[root] = true;
+        while (!queue.isEmpty()) {
+            int cur = queue.poll();
+            for (int next : graph[cur]){
+                if (!visited[next]){
+                    visited[next] = true;
+                    parents[next] = cur;
+                    queue.add(next);
                 }
             }
         }
