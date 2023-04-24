38ece43033356c73340e89ebe6b31fc1b71a1112
Kr.James <115041502+CodingGamePlayer@users.noreply.github.com>
2023-04-24 20:47:56 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 316 ms, Memory: 19336 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true) {
+            String line = br.readLine();
+            if (line.equals(".")) break;
+
+            Stack<Character> stack = new Stack<>();
+            boolean isBalanced = true;
+
+            for (char c : line.toCharArray()) {
+                if (c == '(' || c == '[') {
+                    stack.push(c);
+                } else if (c == ')') {
+                    if (stack.isEmpty() || stack.peek() != '(') {
+                        isBalanced = false;
+                        break;
+                    } else {
+                        stack.pop();
+                    }
+                } else if (c == ']') {
+                    if (stack.isEmpty() || stack.peek() != '[') {
+                        isBalanced = false;
+                        break;
+                    } else {
+                        stack.pop();
+                    }
+                }
+            }
+
+            if (!stack.isEmpty()) {
+                isBalanced = false;
+            }
+
+            if (isBalanced) {
+                System.out.println("yes");
+            } else {
+                System.out.println("no");
+            }
+        }
+    }
+}
