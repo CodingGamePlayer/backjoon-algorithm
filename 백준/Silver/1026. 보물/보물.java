3bf24705f918cbf5fae81798ddad9ced44432b94
Kr.James <115041502+CodingGamePlayer@users.noreply.github.com>
2023-04-24 19:58:04 +0900

    [Silver IV] Title: 보물, Time: 120 ms, Memory: 13060 KB -BaekjoonHub

--- 백준/Silver/1026. 보물/보물.java
+++ 백준/Silver/1026. 보물/보물.java
@@ -1,30 +1,30 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Scanner;
+import java.util.*;
 
-public class Main {
+class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         int n = sc.nextInt();
 
-        ArrayList<Integer> arrA = new ArrayList<>();
-        ArrayList<Integer> arrB = new ArrayList<>();
+        ArrayList<Integer> arr1 = new ArrayList<>();
+        ArrayList<Integer> arr2 = new ArrayList<>();
 
         for (int i = 0; i < n; i++) {
-            arrA.add(sc.nextInt());
+            arr1.add(sc.nextInt());
         }
+
         for (int i = 0; i < n; i++) {
-            arrB.add(sc.nextInt());
+            arr2.add(sc.nextInt());
         }
 
-        Collections.sort(arrA);
-        Collections.sort(arrB,Collections.reverseOrder());
+        Collections.sort(arr1);
+        Collections.sort(arr2, Collections.reverseOrder());
 
-        int answer = 0;
-        for (int i = 0; i < n; i++) {
-            answer += arrA.get(i) * arrB.get(i);
+        int sum = 0;
+        for (int i = 0; i < arr1.size(); i++) {
+            sum += arr1.get(i) * arr2.get(i);
         }
 
-        System.out.println(answer);
+        System.out.println(sum);
     }
 }
