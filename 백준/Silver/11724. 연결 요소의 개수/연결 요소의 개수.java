f2c8f505e3a8e0896005ca3fbaa99f9ee8bebd3e
Kr.James <115041502+CodingGamePlayer@users.noreply.github.com>
2023-04-28 23:44:21 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 636 ms, Memory: 144904 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -1,39 +1,53 @@
-import java.util.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
+
+    static int N;
+    static int M;
     static List<Integer>[] graph;
     static boolean[] visited;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        graph = new ArrayList[n + 1];
-        visited = new boolean[n + 1];
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-        for (int i = 1; i <= n; i++) {
+        graph = new ArrayList[N + 1];
+        visited = new boolean[N + 1];
+
+        for (int i = 0; i < N + 1; i++) {
             graph[i] = new ArrayList<>();
         }
 
-        for (int i = 0; i < m; i++) {
-            int u = sc.nextInt();
-            int v = sc.nextInt();
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+
             graph[u].add(v);
             graph[v].add(u);
         }
 
         int cnt = 0;
-        for (int i = 1; i <= n; i++) {
+        for (int i = 1; i < N + 1; i++) {
             if (!visited[i]) {
-                dfs(i);
                 cnt++;
+                dfs(i);
             }
         }
 
         System.out.println(cnt);
     }
 
-    static void dfs(int node) {
+    private static void dfs(int node) {
+
         visited[node] = true;
         for (int next : graph[node]) {
             if (!visited[next]) {
@@ -41,4 +55,5 @@ public class Main {
             }
         }
     }
+
 }
