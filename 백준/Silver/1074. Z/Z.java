e992848f508a781a3d2e2db207b61c5d21b50c5e
Kr.James <115041502+CodingGamePlayer@users.noreply.github.com>
2023-04-30 20:32:25 +0900

    [Silver I] Title: Z, Time: 84 ms, Memory: 11520 KB -BaekjoonHub

--- 백준/Silver/1074. Z/Z.java
+++ 백준/Silver/1074. Z/Z.java
@@ -1,43 +1,47 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main {
-    static int N, r, c;
-    static int count = 0;
+    static int N, r, c, result;
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        r = sc.nextInt();
-        c = sc.nextInt();
-        sc.close();
 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        r = Integer.parseInt(st.nextToken());
+        c = Integer.parseInt(st.nextToken());
+
+        result = 0;
         int size = (int) Math.pow(2, N);
-        divide(0, 0, size);
+
+        find(0, 0, size);
+
     }
 
-    static void divide(int x, int y, int size) {
+    private static void find(int x, int y, int size) {
         if (size == 1) {
-            if (x == r && y == c) {
-                System.out.println(count);
-            }
-            count++;
+            if (x == r && y == c) System.out.println(result);
+            result++;
             return;
         }
 
-        int half = size / 2;
-        int area = half * half;
-
-        if (r < x + half && c < y + half) {
-            divide(x, y, half);
-        } else if (r < x + half && c >= y + half) {
-            count += area;
-            divide(x, y + half, half);
-        } else if (r >= x + half && c < y + half) {
-            count += area * 2;
-            divide(x + half, y, half);
+        int nSize = size / 2;
+        int num = nSize * nSize;
+        if (r < nSize + x && c < nSize + y) {
+            find(x, y, nSize);
+        } else if (r < nSize + x && c >= nSize + y) {
+            result += num;
+            find(x, y + nSize, nSize);
+        } else if (r >= nSize + x && c < nSize + y) {
+            result += num * 2;
+            find(x + nSize, y, nSize);
         } else {
-            count += area * 3;
-            divide(x + half, y + half, half);
+            result += num * 3;
+            find(x + nSize, y + nSize, nSize);
         }
     }
 }
