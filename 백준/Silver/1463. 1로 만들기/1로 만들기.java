6c9449ac8c49e21a3e48b368c2120f41b786b9ed
Kr.James <115041502+CodingGamePlayer@users.noreply.github.com>
2023-05-02 20:32:10 +0900

    [Silver III] Title: 1로 만들기, Time: 104 ms, Memory: 15512 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,19 +1,20 @@
-import java.util.Scanner;
+
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
 
-        System.out.println(solution(n));
-    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//        StringTokenizer st = null;
 
-    private static int solution(int n) {
+        int n = Integer.parseInt(br.readLine());
+
         int[] dp = new int[n + 1];
 
-        for (int i = 2; i <= n; i++) {
+        for (int i = 2; i <= n ; i++) {
             dp[i] = dp[i - 1] + 1;
-
             if (i % 2 == 0) {
                 dp[i] = Math.min(dp[i], dp[i / 2] + 1);
             }
@@ -23,6 +24,6 @@ public class Main {
             }
         }
 
-        return dp[n];
+        System.out.println(dp[n]);
     }
 }
